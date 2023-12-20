f89816400b9ed11f9c282d791a65a48e7b543afc
HIROSE Masaya <hirose@asial.co.jp>
2023-12-20 14:07:51 +0900

    fix: getAuth() error in iOS

--- www/app.js
+++ www/app.js
@@ -1,6 +1,6 @@
 // FirebaseのSDKをインポート
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getAuth, connectAuthEmulator, sendPasswordResetEmail, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+import { getAuth, connectAuthEmulator, sendPasswordResetEmail, signInWithEmailAndPassword, onAuthStateChanged, signOut, initializeAuth, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 // Firebaseの設定
 const firebaseConfig = {
@@ -8,34 +8,25 @@ const firebaseConfig = {
 };
 
 // Firebaseの初期化
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+// iOSを判定する関数
+const isIos = () => {
+    const userAgent = navigator.userAgent;
+    return /iPad|iPhone|iPod/i.test(userAgent);
+}
 
 // Authenticationのインスタンスを取得
-const auth = getAuth();
-auth.languageCode = 'ja'; // パスワードリセットメールやパスワードリセット画面の言語設定
-// connectAuthEmulator(auth, "http://127.0.0.1:9099"); // Emulatorを起動する場合、コメントを外す
-
-// ログイン状態が変わった時に実行される処理
-onAuthStateChanged(auth, (user) => {
-    renderLoginStatus(user);
-});
-
-// ログイン状態を画面に反映させる関数
-const renderLoginStatus = (user) => {
-    const loginStatus = document.getElementById("loginStatus");
-    let html = `<p>未ログイン</p>`;
-    if (user) {
-        html = `
-            <p>ログイン中</p>
-            <p>ユーザー名：${user.displayName}</p>
-            <p>メールアドレス：${user.email}</p>
-            <p>ユーザーID：${user.uid}</p>
-            <button class="button" id="logoutButton">ログアウト</button>
-        `;
-    }
-    loginStatus.innerHTML = html;
-    document.getElementById('logoutButton').addEventListener('click', onClickLogout);
+let auth;
+if (isIos()) {
+    auth = initializeAuth(app, {
+        persistence: browserLocalPersistence,
+    })
+} else {
+    auth = getAuth(app);
 }
+auth.languageCode = "ja"; // パスワードリセットメールやパスワードリセット画面の言語設定
+// connectAuthEmulator(auth, "http://127.0.0.1:9099"); // Emulatorを起動する場合、コメントを外す
 
 // パスワード再設定メールを送信ボタンがクリックされたときの処理
 const onClickSendPasswordResetEmail = () => {
@@ -66,6 +57,30 @@ const onClickLogin = () => {
             console.error(error);
         });
 };
+
+// ログイン状態が変わった時に実行される処理
+onAuthStateChanged(auth, (user) => {
+    renderLoginStatus(user);
+});
+
+// ログイン状態を画面に反映させる関数
+const renderLoginStatus = (user) => {
+    const loginStatus = document.getElementById("loginStatus");
+    let html = `<p>未ログイン</p>`;
+    if (user) {
+        html = `
+            <p>ログイン中</p>
+            <p>ユーザー名：${user.displayName}</p>
+            <p>メールアドレス：${user.email}</p>
+            <p>ユーザーID：${user.uid}</p>
+            <button class="button" id="logoutButton">ログアウト</button>
+        `;
+    }
+    loginStatus.innerHTML = html;
+    if (user) {
+        document.getElementById("logoutButton").addEventListener("click", onClickLogout);
+    }
+}
 
 // ログアウトボタンがクリックされたときの処理
 const onClickLogout = () => {
